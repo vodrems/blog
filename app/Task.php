e29e6929ed5e7b400dbf7159aa8f3d0b147d95a1
Andrew Smerdov <andrew.smerdov@gmail.com>
2018-03-14 20:04:18 +0200

    make relations with tasks and user

--- app/Task.php
+++ app/Task.php
@@ -6,5 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class Task extends Model
 {
-    //
+    /**
+     * @var array
+     */
+    protected $fillable = [
+        'name'
+    ];
+
+    public function user() {
+        return $this->belongsTo(User::class);
+    }
 }
